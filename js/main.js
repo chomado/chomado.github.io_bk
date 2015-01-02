2b2f381b8c1c048a1dfc1a25ec8ba659470dfc44
chomado <chomado@gmail.com>
2015-01-03 02:15:28 +0900

    クマさん引退させてchomadoアイコン転がすことにした

--- js/main.js
+++ js/main.js
@@ -5,30 +5,24 @@ window.onload = function() {
 	// 画面サイズ決定
 	var core = new Core(1024, 640);
 	
-	core.preload('image/chara1.png');
+	core.preload('image/chomado.jpg');
 	
 	// fps = frame per second
 	core.fps = 15;
 
 	core.onload = function() {
-		// 動かすクマさんとその画像の大きさ
-		var bear = new Sprite(32, 32);
-		bear.image = core.assets['image/chara1.png'];
+		// 動かす画像の大きさ
+		var icon = new Sprite(256, 256);
+		icon.image = core.assets['image/chomado.jpg'];
 		
 		// 初期位置座標
-		bear.x = 0;
-		bear.y = 0;
+		icon.x = 0;
+		icon.y = 0;
 
-		// chara1.png (13コマある) のうちどれを使うか
-		bear.frame = 1;
-		
-		bear.addEventListener('enterframe', function() {
-			// 5ピクセルずつ動かす.(数字が大きいと速くなる)
+		icon.addEventListener('enterframe', function() {
 			this.x += 5;
-			// 歩かせたいので1,2(画像chara1.pngの各フレーム番号)が交互に来るようにする.
-			this.frame = this.age % 3;
-			// 大きさ
-			this.scale(1.005, 1.005);
+			this.rotate(2);
+			this.scale(1.01, 1.01);
 
 			// 無限ループさせる
 			if (this.x > 1024) 
@@ -36,7 +30,7 @@ window.onload = function() {
 				this.x = 0;
 			}
 		});
-		core.rootScene.addChild(bear);
+		core.rootScene.addChild(icon);
 	}
 	core.start();
 };
