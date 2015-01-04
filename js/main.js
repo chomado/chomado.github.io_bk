42e5e8a566dac3fdf43d3a8a9144c2f5e71b626b
chomado <chomado@gmail.com>
2015-01-04 22:14:23 +0900

    矢印キーでクマを動かす. タッチした所にクマ移動. クマをタッチするとクマ消滅

--- js/main.js
+++ js/main.js
@@ -3,33 +3,41 @@ enchant();
 window.onload = function() {
 	
 	// 画面サイズ決定
-	var core = new Core(1024, 640);
+	var core = new Core(1024, 1024);
 	
-	core.preload('image/chomado.jpg');
+	core.preload('image/chara1.png');
 	
 	// fps = frame per second
 	core.fps = 15;
 
 	core.onload = function() {
 		// 動かす画像の大きさ
-		var icon = new Sprite(256, 256);
-		icon.image = core.assets['image/chomado.jpg'];
+		var icon = new Sprite(32, 32);
+		icon.image = core.assets['image/chara1.png'];
 		
 		// 初期位置座標
 		icon.x = 0;
 		icon.y = 0;
 
 		icon.addEventListener('enterframe', function() {
-			this.x += 5;
-			this.rotate(2);
-			this.scale(1.01, 1.01);
-
-			// 無限ループさせる
-			if (this.x > 1024) 
-			{
-				this.x = 0;
-			}
+			if (core.input.left)
+				this.x -= 5;
+			if (core.input.right)
+				this.x += 5;
+			if (core.input.up)
+				this.y -= 5;
+			if (core.input.down)
+				this.y += 5;
+			
 		});
+		icon.on('touchstart', function() {
+			core.rootScene.removeChild(this);
+		});
+		core.rootScene.on('touchstart', function(e) {
+			icon.x = e.x;
+			icon.y = e.y;
+		});
+
 		core.rootScene.addChild(icon);
 	}
 	core.start();
